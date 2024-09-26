50567991168ec35ecb06ee6035b505edc3d3badd
Kevin Hu <hellokevinhu@icloud.com>
2024-09-26 17:00:44 +0800

    Add type decorator to dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,4 +1,5 @@
 import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class createProductDto {
   @IsString()
@@ -10,5 +11,6 @@ export class createProductDto {
   description: string;
 
   @IsNumber()
+  @Type(() => Number)
   price: number;
 }
